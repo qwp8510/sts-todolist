21a406bc5d948d531a4e4a28b5ad591025d2a7cb
qwp8510 <a4793706@gmail.com>
2025-02-10 00:20:02 +0800

    chore: update db port setting

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const envPath = "./env";
 dotenvExpand.expand(dotenv.config({ path: envPath }));
 
 export const DB_HOST = process.env.DB_HOST || '0.0.0.0';
-export const DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 54321;
+export const DB_PORT = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 54322;
 export const DB_USERNAME = process.env.DB_USERNAME || 'postgres';
 export const DB_PWD = process.env.DB_PWD || 'abc123';
 export const DB_NAME = process.env.DB_NAME || 'postgres';
