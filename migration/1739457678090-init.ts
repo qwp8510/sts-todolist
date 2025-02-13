8c69b28dd6e1ef2afa42b435621dec5778173ed1
qwp8510 <a4793706@gmail.com>
2025-02-13 22:42:31 +0800

    feat: add init migration

--- migration/1739457678090-init.ts
+++ migration/1739457678090-init.ts
@@ -1,18 +1,17 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1739205586689 implements MigrationInterface {
-    name = 'Init1739205586689'
+export class Init1739457678090 implements MigrationInterface {
+    name = 'Init1739457678090'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "teams" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, CONSTRAINT "PK_7e5523774a38b08a6236d322403" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "team_members" ("id" SERIAL NOT NULL, "role" character varying(20) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "teamId" integer, "userId" integer, CONSTRAINT "PK_ca3eae89dcf20c9fd95bf7460aa" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "tasks" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "team_id" integer NOT NULL, "parent_id" integer, "title" character varying(255) NOT NULL, "description" text, "status" character varying(20) NOT NULL, "due_date" TIMESTAMP, "creator_id" integer, CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "task_assignees" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "task_id" integer, "user_id" integer, CONSTRAINT "PK_e23bc1438f7bb32f41e8d493e78" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "task_history" ("id" SERIAL NOT NULL, "action" character varying(50) NOT NULL, "comment" text, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "task_id" integer, "user_id" integer, CONSTRAINT "PK_716670443aea4a2f4a599bb7c53" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "task_watchers" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "task_id" integer, "user_id" integer, CONSTRAINT "PK_fa77927e9a914d3ed0b6726a6e2" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "team_members" ADD CONSTRAINT "FK_6d1c8c7f705803f0711336a5c33" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "team_members" ADD CONSTRAINT "FK_0a72b849753a046462b4c5a8ec2" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`CREATE TABLE "tasks" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "team_id" integer NOT NULL, "parent_id" integer, "title" character varying(255) NOT NULL, "description" text, "status" character varying(20) NOT NULL, "due_date" TIMESTAMP, "creator_id" integer, CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_707cfc415c7c12d38dfc2ec8eb" ON "tasks" ("due_date") `);
+        await queryRunner.query(`CREATE TABLE "task_assignees" ("id" SERIAL NOT NULL, "task_id" integer NOT NULL, "user_id" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e23bc1438f7bb32f41e8d493e78" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "task_history" ("id" SERIAL NOT NULL, "task_id" integer NOT NULL, "user_id" integer NOT NULL, "action" character varying(50) NOT NULL, "comment" text, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_716670443aea4a2f4a599bb7c53" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "task_watchers" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "task_id" integer NOT NULL, "user_id" integer NOT NULL, CONSTRAINT "PK_fa77927e9a914d3ed0b6726a6e2" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "teams" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "name" character varying NOT NULL, CONSTRAINT "PK_7e5523774a38b08a6236d322403" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "team_members" ("id" SERIAL NOT NULL, "role" character varying(20) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "teamId" integer, "userId" integer, CONSTRAINT "PK_ca3eae89dcf20c9fd95bf7460aa" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "tasks" ADD CONSTRAINT "FK_2b1604aae04e0dec6e38580e099" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "tasks" ADD CONSTRAINT "FK_f4cb489461bc751498a28852356" FOREIGN KEY ("creator_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "tasks" ADD CONSTRAINT "FK_b03c99063a4eaf084f069a4d5a7" FOREIGN KEY ("parent_id") REFERENCES "tasks"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -22,9 +21,13 @@ export class Init1739205586689 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "task_history" ADD CONSTRAINT "FK_b0fefc7c578afbdb1e749dee727" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "task_watchers" ADD CONSTRAINT "FK_6ec69c77281f4bc0a18fdbc722a" FOREIGN KEY ("task_id") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "task_watchers" ADD CONSTRAINT "FK_ff976f3fc177614cc4a60291de1" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "team_members" ADD CONSTRAINT "FK_6d1c8c7f705803f0711336a5c33" FOREIGN KEY ("teamId") REFERENCES "teams"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "team_members" ADD CONSTRAINT "FK_0a72b849753a046462b4c5a8ec2" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "team_members" DROP CONSTRAINT "FK_0a72b849753a046462b4c5a8ec2"`);
+        await queryRunner.query(`ALTER TABLE "team_members" DROP CONSTRAINT "FK_6d1c8c7f705803f0711336a5c33"`);
         await queryRunner.query(`ALTER TABLE "task_watchers" DROP CONSTRAINT "FK_ff976f3fc177614cc4a60291de1"`);
         await queryRunner.query(`ALTER TABLE "task_watchers" DROP CONSTRAINT "FK_6ec69c77281f4bc0a18fdbc722a"`);
         await queryRunner.query(`ALTER TABLE "task_history" DROP CONSTRAINT "FK_b0fefc7c578afbdb1e749dee727"`);
@@ -34,15 +37,14 @@ export class Init1739205586689 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "tasks" DROP CONSTRAINT "FK_b03c99063a4eaf084f069a4d5a7"`);
         await queryRunner.query(`ALTER TABLE "tasks" DROP CONSTRAINT "FK_f4cb489461bc751498a28852356"`);
         await queryRunner.query(`ALTER TABLE "tasks" DROP CONSTRAINT "FK_2b1604aae04e0dec6e38580e099"`);
-        await queryRunner.query(`ALTER TABLE "team_members" DROP CONSTRAINT "FK_0a72b849753a046462b4c5a8ec2"`);
-        await queryRunner.query(`ALTER TABLE "team_members" DROP CONSTRAINT "FK_6d1c8c7f705803f0711336a5c33"`);
-        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "team_members"`);
+        await queryRunner.query(`DROP TABLE "teams"`);
         await queryRunner.query(`DROP TABLE "task_watchers"`);
         await queryRunner.query(`DROP TABLE "task_history"`);
         await queryRunner.query(`DROP TABLE "task_assignees"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_707cfc415c7c12d38dfc2ec8eb"`);
         await queryRunner.query(`DROP TABLE "tasks"`);
-        await queryRunner.query(`DROP TABLE "team_members"`);
-        await queryRunner.query(`DROP TABLE "teams"`);
+        await queryRunner.query(`DROP TABLE "users"`);
     }
 
 }
